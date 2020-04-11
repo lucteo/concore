1cd60ef59479d64c4a193c7db32c868e991fc72b
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2020-04-11 19:12:04 +0300

    Document task_group, spawn and task_graph

--- include/concore/spawn.hpp
+++ include/concore/spawn.hpp
@@ -23,26 +23,71 @@ struct spawn_continuation_executor {
 
 inline namespace v1 {
 
-//! Spawns a new task in the current worker thread.
-//! It is assumed this is going to be called from within a task (within a worker thread); in this
-//! case, the task will be added to the list of tasks for the current worker.
-//!
-//! If the 'wake_workers' parameter is true, this will attempt to wake up workers to ensure that the
-//! task is executed as soon as possible. But, if this is "a continuation" of the parent task, this
-//! may not make sense, as we are moving computation to a different thread; for such cases, one can
-//! pass false to the wake_workers parameter.
+/**
+ * @brief      Spawns a task, hopefully in the current working thread.
+ *
+ * @param      t             The task to be spawned
+ * @param      wake_workers  True if we should wake other workers for this task
+ * 
+ * This is intended to be called from within a task. In this case, the task will be added to the
+ * list of tasks for the current worker thread. The tasks will be added in the front of the list, so
+ * it will be executed in front of others.
+ * 
+ * The add-to-front strategy aims as improving locality of execution. We assume that this task is
+ * closer to the current task than other tasks in the system.
+ * 
+ * If the current running task does not finish execution after spawning this new task, it's advised
+ * for the `wake_workers` parameter to be set to `true`. If, on the other hand, the current task
+ * finishes execution after this, it may be best to not set `wake_workers` to `false` and thus try
+ * to wake other threads. Waking up other threads can be an efficiency loss that we don't need if we
+ * know that this thread is finishing soon executing the current task.
+ * 
+ * Note that the given task ca take a task_group at construction. This way, the users can control
+ * the groups of the spawned tasks.
+ */
 inline void spawn(task&& t, bool wake_workers = true) {
     detail::task_system::instance().spawn(std::move(t), wake_workers);
 }
 
-//! Spawn one task, given a functor to be executed.
+/**
+ * @brief      Spawn one task, given a functor to be executed.
+ *
+ * @param      ftor          The ftor to be executed
+ * @param      wake_workers  True if we should wake other workers for this task
+ *
+ * @tparam     F             The type of the functor
+ * 
+ * This is similar to the @ref spawn(task&&, bool) function, but it takes directly a functor instead
+ * of a task.
+ * 
+ * If the current task has a group associated, the new task will inherit that group.
+ * 
+ * @see spawn(task&&, bool)
+ */
 template <typename F>
 inline void spawn(F&& ftor, bool wake_workers = true) {
     auto grp = task_group::current_task_group();
     detail::task_system::instance().spawn(task(std::forward<F>(ftor), grp), wake_workers);
 }
 
-//! Spawn multiple tasks, given the functors to be executed.
+/**
+ * @brief      Spawn multiple tasks, given the functors to be executed.
+ *
+ * @param      ftors         A list of functors to be executed
+ * @param      wake_workers  True if we should wake other workers for the last task
+ * 
+ * This is similar to the other two @ref spawn() functions, but it takes a series of functions to be
+ * executed. Tasks will be created for all these functions and spawn accordingly.
+ * 
+ * The `wake_workers` will control whether to wake threads for the last task or not. For the others
+ * tasks, it is assumed that we always want to wake other workers to attempt to get as many tasks as
+ * possible from the current worker task list.
+ * 
+ * If the current task has a task group associated, all the newly created tasks will inherit that 
+ * group.
+ * 
+ * @ref spawn(task&&, bool), spawn_and_wait()
+ */
 inline void spawn(std::initializer_list<task_function>&& ftors, bool wake_workers = true) {
     auto grp = task_group::current_task_group();
     int count = static_cast<int>(ftors.size());
@@ -53,6 +98,22 @@ inline void spawn(std::initializer_list<task_function>&& ftors, bool wake_worker
     }
 }
 
+/**
+ * @brief      Spawn a task and wait for it.
+ *
+ * @param      ftor  The functor of the tasks to be spawned
+ *
+ * @tparam     F     The type of the functor.
+ * 
+ * This function is similar to the @ref spawn() functions, but, after spawning, also waits for the
+ * spawned task to complete. This wait is an active-wait, as it tries to execute other tasks. In
+ * principle, the current thread executes the spawn task.
+ * 
+ * This will create a new task group, inheriting from the task group of the currently executing task
+ * and add the new task in this new group. The waiting is done on this new group.
+ * 
+ * @see spawn()
+ */
 template <typename F>
 inline void spawn_and_wait(F&& ftor) {
     auto& tsys = detail::task_system::instance();
@@ -65,6 +126,21 @@ inline void spawn_and_wait(F&& ftor) {
     tsys.exit_worker(worker_data);
 }
 
+/**
+ * @brief      Spawn multiple tasks and wait for them to complete
+ *
+ * @param      ftors         A list of functors to be executed
+ * @param      wake_workers  True if we should wake other workers for the last task
+ * 
+ * This is used when a task needs multiple things done in parallel.
+ * 
+ * This function is similar to the @ref spawn() functions, but, after spawning, also waits for the
+ * spawned tasks to complete. This wait is an active-wait, as it tries to execute other tasks. In
+ * principle, the current thread executes the last of the spawned tasks.
+ * 
+ * This will create a new task group, inheriting from the task group of the currently executing task
+ * and add the new tasks in this new group. The waiting is done on this new group.
+ */
 inline void spawn_and_wait(std::initializer_list<task_function>&& ftors, bool wake_workers = true) {
     auto& tsys = detail::task_system::instance();
     auto worker_data = tsys.enter_worker();
@@ -80,14 +156,38 @@ inline void spawn_and_wait(std::initializer_list<task_function>&& ftors, bool wa
     tsys.exit_worker(worker_data);
 }
 
-//! Wait on all the tasks from the given group to finish.
-//! Keeping a children task alive from the group will make this wait forever.
+/**
+ * @brief      Wait on all the tasks in the given group to finish executing.
+ *
+ * @param      grp   The task group to wait on
+ * 
+ * The wait here is an active-wait. This will execute tasks from the task system in the hope that
+ * the tasks in the group are executed faster.
+ * 
+ * Using this inside active tasks is not going to block the worker thread and thus not degrade
+ * performance.
+ * 
+ * @warning    If one adds task in a group and never executes them, this function will block
+ *             indefinitely.
+ *
+ * @see spawn(), spawn_and_wait()
+ */
 inline void wait(task_group& grp) { detail::task_system::instance().busy_wait_on(grp); }
 
-//! Executor that spawns tasks instead of enqueueing them
+/**
+ * Executor that spawns tasks instead of enqueueing them.
+ * Similar to calling @ref spawn() on the task.
+ * 
+ * @see spawn(), spawn_continuation_executor, global_executor
+ */
 constexpr auto spawn_executor = detail::spawn_executor{};
-//! Executor that spawns tasks instead of enqueueing them; this doesn't wake up other workers, so
-//! the task is assumed to be a continuation of the current work.
+
+/**
+ * Executor that spawns tasks instead of enqueueing them, but not waking other workers.
+ * Similar to calling `spawn(task, false)` on the task.
+ * 
+ * @see spawn(), spawn_executor, global_executor
+ */
 constexpr auto spawn_continuation_executor = detail::spawn_continuation_executor{};
 
 } // namespace v1
