50ea0a4e5c4ab86d307bab576167e2445af039ca
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2020-09-12 13:49:08 +0300

    Test just gcc compilation

---
+++ .github/workflows/test-clang.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+CURDIR=$(realpath $(dirname "$0"))
+source ${CURDIR}/_test_common.sh
+
+# Run docker with action-cxx-toolkit to check our code
+docker run ${DOCKER_RUN_PARAMS} -e INPUT_CC='clang'
+status=$?
+printStatus $status
