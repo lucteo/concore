1e7f95aed126577f6ea8df9097c5a8a379045ab6
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2020-09-13 20:44:38 +0300

    Fix in clang-tidy and clang-format checks

--- .github/workflows/test-static-checks.sh
+++ .github/workflows/test-static-checks.sh
@@ -9,6 +9,6 @@ source ${CURDIR}/_test_common.sh
 startTest "Static checks test"
 
 # Run docker with action-cxx-toolkit to check our code
-docker run ${DOCKER_RUN_PARAMS} -e INPUT_CC='gcc-9' -e INPUT_CHECKS='clang-tidy' action-cxx-toolkit
+docker run ${DOCKER_RUN_PARAMS} -e INPUT_CC='gcc-9' -e INPUT_CHECKS='iwyu cppcheck clang-tidy' action-cxx-toolkit
 status=$?
 printStatus $status
