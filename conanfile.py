a3dce93c7adee6a4cb0d3d861c1a71e0c1ca3991
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2021-06-01 14:04:20 +0300

    Fixes to allow building conan package

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,15 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
+import re
+from os import path
 
 class ConcoreRecipe(ConanFile):
    name = "concore"
    description = "Core abstractions for dealing with concurrency in C++"
    author = "Lucian Radu Teodorescu"
-   version = "0.6.0"
    topics = ("concurrency", "tasks", "executors", "no-locks")
    homepage = "https://github.com/lucteo/concore"
+   url = "https://github.com/lucteo/concore"
    license = "MIT"
-
-   requires = "catch2/2.13.6", "rapidcheck/20210107", "benchmark/1.5.3"
 
    settings = "os", "compiler", "build_type", "arch"
    generators = "cmake"
@@ -21,12 +21,29 @@ class ConcoreRecipe(ConanFile):
    exports = "LICENSE"
    exports_sources = ("src/*", "include/*", "CMakeLists.txt")
 
+   def set_version(self):
+      # Get the version from src/CMakeList.txt project definition
+      content = tools.load(path.join(self.recipe_folder, "src/CMakeLists.txt"))
+      version = re.search(r"project\([^\)]+VERSION (\d+\.\d+\.\d+)[^\)]*\)", content).group(1)
+      self.version = version.strip()
+
+   @property
+   def _run_tests(self):
+       return tools.get_env("CONAN_RUN_TESTS", False)
+
+   def build_requirements(self):
+      if self._run_tests:
+         self.build_requires("catch2/2.13.6")
+         self.build_requires("rapidcheck/20210107")
+         self.build_requires("benchmark/1.5.3")
+
    def config_options(self):
        if self.settings.os == "Windows":
            del self.options.fPIC
 
    def configure(self):
       # TODO: do we want to provide a header+inline source version of the library? If yes, this is the place to do it
+      # TODO: shared vs static library
       pass
 
    def build(self):
@@ -34,23 +51,17 @@ class ConcoreRecipe(ConanFile):
       cmake = self._configure_cmake()
       cmake.build()
 
-   # TODO: finalize packaging
    def package(self):
-      # self.copy("*")
-      self.copy("LICENSE")
-      self.copy("src/*")
-      self.copy("include/*")
+      self.copy(pattern="LICENSE", dst="licenses")
+      cmake = self._configure_cmake()
+      cmake.install()
 
    def package_info(self):
       self.cpp_info.libs = self.collect_libs()
 
    def _configure_cmake(self):
       cmake = CMake(self)
-      cmake.definitions["CONCORE_BUILD_TESTS"] = "OFF"
-      cmake.definitions["CONCORE_BUILD_EXAMPLES"] = "OFF"
-      cmake.definitions["CONCORE_INSTALL_DOCS"] = "OFF"
-      cmake.definitions["CONCORE_INSTALL_HELPERS"] = "ON"
-      cmake.configure(build_folder="build")
+      cmake.configure(source_folder=None if self._run_tests else "src")
       return cmake
 
 
