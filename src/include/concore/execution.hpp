3a047471a7fc207da76f86ca80799d24af398535
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2021-12-29 23:44:34 +0200

    Ignored warning coming from P2300 execution

--- src/include/concore/execution.hpp
+++ src/include/concore/execution.hpp
@@ -3,7 +3,17 @@
 #include <concore/detail/cxx_features.hpp>
 
 #if CONCORE_USE_CXX2020 && CONCORE_CPP_VERSION >= 20
+
+#if CONCORE_CPP_COMPILER(gcc)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
+#endif
+
 #include <concore/_gen/c++20/execution.hpp>
+
+#if CONCORE_CPP_COMPILER(gcc)
+#pragma GCC diagnostic pop
+#endif
 
 namespace  concore {
     namespace execution = concore::_p2300::execution;
